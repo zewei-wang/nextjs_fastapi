fc6d90b9e67feae62ae867df09d341a9f7816056
Rocker <rocker@xxx.com>
2024-11-09 00:14:32 -0500

    export to out file

--- next.config.js
+++ next.config.js
@@ -1,20 +1,21 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  rewrites: async () => {
-    return [
-      {
-        source: "/api/:path*",
-        destination:
-          process.env.NODE_ENV === "development"
-            ? "http://127.0.0.1:8000/api/:path*"
-            : "/api/",
-      },
-    ];
-  },
-};
+// const nextConfig = {
+//   output: 'export',
+//   rewrites: async () => {
+//     return [
+//       {
+//         source: "/api/:path*",
+//         destination:
+//           process.env.NODE_ENV === "development"
+//             ? "http://127.0.0.1:8000/api/:path*"
+//             : "/api/",
+//       },
+//     ];
+//   },
+// };
 
 // module.exports = nextConfig;
 
 module.exports = {
-  output: "standalone",
+  output: "export",
 };
